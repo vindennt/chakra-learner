b6b11dfeacb40dac0863b1eeedffd5a997440e8c
dennistruong <truong.dennis00@gmail.com>
2023-03-07 12:10:27 -0800

    test implement markdown for chakra

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,13 @@
-import { useColorMode } from "@chakra-ui/react";
+import { getSortedPostsData } from "/lib/posts";
+
+import utilStyles from "../styles/utils.module.css";
+// import Date from "../components/date";
+// import Layout from "../components/layout";
+import React from "react";
+import ChakraUIRenderer from "chakra-ui-markdown-renderer";
+import ReactMarkdown from "react-markdown";
+import ReactDom from "react-dom";
+
 import {
   Box,
   Flex,
@@ -9,11 +18,12 @@ import {
   useColorModeValue,
   Stack,
   Text,
+  useColorMode,
 } from "@chakra-ui/react";
 import { Heading } from "@chakra-ui/react";
-
 import { FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
 import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+
 const Links = ["About me", "Projects", "Blog", "Art", "Contact"];
 const NavLink = ({ children }, { children: ReactNode }) => (
   <Link
@@ -29,6 +39,8 @@ const NavLink = ({ children }, { children: ReactNode }) => (
     {children}
   </Link>
 );
+
+// const allPostsData = getSortedPostsData();
 
 export default function Simple() {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -82,7 +94,7 @@ export default function Simple() {
       </Box>
 
       <Box p={4}>
-        <Flex height="20vh" alignItems="center" justifyContent="center">
+        <Flex height="20vh" alignItems="center" direction="column">
           <Flex
             direction="column"
             background="grey.100"
@@ -93,10 +105,48 @@ export default function Simple() {
           >
             <Heading mb={6}>Dennis Truong</Heading>
             <Text mb={6}>
-              Web developer with experience in JavaScript, Java, C++, and
+              Web developer with experience in JavaScript, Java, C+ +, and
               Python.
             </Text>
+            <ReactMarkdown
+              components={ChakraUIRenderer()}
+              children={"## Experience"}
+              skipHtml
+            />
+            <Flex
+              direction="column"
+              background="grey.100"
+              p={12}
+              rounded={6}
+              alignItems="center"
+              justifyContent="top"
+            >
+              <ReactMarkdown
+                components={ChakraUIRenderer()}
+                children={
+                  "- Lorem ipsum dolor, sit amet consecteturadipisicing"
+                }
+                skipHtml
+              />
+            </Flex>
           </Flex>
+
+          {/* <section
+            className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}
+          >
+            <h2 className={utilStyles.headingLg}>Blog</h2>
+            <ul className={utilStyles.list}>
+              {allPostsData.map(({ id, date, title }) => (
+                <li className={utilStyles.listItem} key={id}>
+                  <Link href={`/posts/${id}`}>{title}</Link>
+                  <br />
+                  <small className={utilStyles.lightText}>
+                    <Date dateString={date} />
+                  </small>
+                </li>
+              ))}
+            </ul>
+          </section> */}
         </Flex>
       </Box>
     </>
